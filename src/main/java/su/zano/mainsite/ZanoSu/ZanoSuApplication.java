db8c2128249ad1073c090bafc22faf15a06b5286
IvanSilaev <isilaev@akolchin.com>
2017-12-27 22:00:25 +0300

    Базовая структура проекта

---
+++ src/main/java/su/zano/mainsite/ZanoSu/ZanoSuApplication.java
@@ -0,0 +1,12 @@
+package su.zano.mainsite.ZanoSu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanoSuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZanoSuApplication.class, args);
+	}
+}
---
+++ src/test/java/su/zano/mainsite/ZanoSu/ZanoSuApplicationTests.java
@@ -0,0 +1,16 @@
+package su.zano.mainsite.ZanoSu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZanoSuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
